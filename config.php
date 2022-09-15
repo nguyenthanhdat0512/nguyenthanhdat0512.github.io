03d8c52fcf38aaccc6cd822b70b203e139448470
Rokerket <113525181+Rokerket@users.noreply.github.com>
2022-09-15 20:06:36 +0700

    upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define("HOST_DB", 'localhost:3306');
+define("USER_DB", 'root');
+define("PASS_DB", '');
+define("DB", 'bookstore');
+
+define ('ROOT', dirname(__FILE__));
+define('BASE_URL', 'http://localhost/MVC3/');
+define('UPLOAD_IMG', ROOT .'/images/book');
+
